2bebaf9461df33923f4a2b4aa62be7f7914df41a
FYUU <42712871+FYUU@users.noreply.github.com>
2019-07-16 22:45:56 +0800

    Update Demo.java

--- demo/src/demo/Demo.java
+++ demo/src/demo/Demo.java
@@ -6,7 +6,8 @@ public class Demo {
 		// TODO Auto-generated method stub
 		System.out.println("李子晨好帅");
 		System.out.println("龙哥威武");
-		system.out.println("大家都来过吗);
+		system.out.println("大家都来过吗");
+		system.out.println("三组的大佬们都不登录GitHub的吗？");
 	}
 
 }
